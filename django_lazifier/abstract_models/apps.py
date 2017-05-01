c4d4005e9e825431f75a1b11b24c264642512b2e
Du <hotmit@gmail.com>
2017-05-01 15:25:53 -0400

    split the default query set into individual classes

---
+++ django_lazifier/abstract_models/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AbstractModelsConfig(AppConfig):
+    name = 'abstract_models'
--- django_lazifier/abstract_models/models.py
+++ django_lazifier/abstract_models/models.py
@@ -3,31 +3,46 @@ from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
 
 
-class BaseQuerySet(models.QuerySet):
+class TimeStampQuerySet(models.QuerySet):
     def created_between(self, start, end):
         return self.filter(created__range=(start, end))
 
+    def modified_between(self, start, end):
+        return self.filter(modified__range=(start, end))
+
+
+class EnabledQuerySet(models.QuerySet):
     def enabled(self):
         return self.filter(enabled=True)
 
     def disabled(self):
         return self.filter(enabled=False)
 
+
+class ActiveQuerySet(models.QuerySet):
     def active_records(self):
         return self.filter(is_active=True)
 
     def inactive_records(self):
         return self.filter(is_active=False)
 
+
+class OwnerQuerySet(models.QuerySet):
     def by_owner(self, owner):
         if type(owner) is not int:
             owner = getattr(owner, 'id')
         return self.filter(owner_id=owner)
 
 
+class BaseQuerySet(TimeStampQuerySet, EnabledQuerySet, ActiveQuerySet, OwnerQuerySet):
+    def active_by_owner(self, owner):
+        filter = self.active_records()
+        return filter.by_owner(owner)
+
+
 class TimestampModel(models.Model):
-    created = models.DateTimeField(auto_now_add=True, verbose_name=_('created timestamped'))
-    modified = models.DateTimeField(auto_now=True, verbose_name=_('modified timestamped'))
+    created = models.DateTimeField(auto_now_add=True, verbose_name=_('created'))
+    modified = models.DateTimeField(auto_now=True, verbose_name=_('last modified'))
 
     objects = BaseQuerySet.as_manager()
 
